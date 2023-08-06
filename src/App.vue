9edcc1b964433ba3e0f67c29f1f5e3bef465f08f
obs1di4n <80841181+fil1pko@users.noreply.github.com>
2023-08-06 19:00:27 +0200

    Default app plus trash icon plus starting menu

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import navbar from './components/navbar/navbar.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
+    <navbar/>
+    <div class="wrapper">
+      <h1>super apka funguje</h1>
+      <!-- <HelloWorld msg="You did it!" /> -->
+    </div>
+  </header>
+
+  <main>
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
+
+<style lang="scss">
+  @import './assets/style.scss';
+  @import './assets/styles/colors.scss';
+
+</style>
---
+++ src/components/navbar/navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <ul>
+            <hr>
+            <li><a href="#home">e</a></li>
+            <hr>
+            <li><a href="#about"></a></li>
+            <hr>
+            <li><a href="#services"></a></li>
+            <hr>
+            <li><a href="#contact"></a></li>
+            <hr>
+        </ul>
+    </nav> 
+</template>
