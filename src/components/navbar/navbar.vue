4998672d498597721cc001be04b083162871b396
obs1di4n <80841181+fil1pko@users.noreply.github.com>
2023-08-07 01:13:21 +0200

    navigation items and active item glowing

--- src/components/navbar/navbar.vue
+++ src/components/navbar/navbar.vue
@@ -1,22 +1,51 @@
 <template>
     <header>
         <nav>
-            <ul>
-                <hr>
-                <li><a href="#home">e</a></li>
-                <hr>
-                <li><a href="#about"></a></li>
-                <hr>
-                <li><a href="#services"></a></li>
-                <hr>
-                <li><a href="#contact"></a></li>
-                <hr>
-            </ul>
-        </nav> 
+      <!-- left menu side -->
+      <ul>
+        <span><hr class="long"></span>
+        <li><a @click="scrollToSection('home')" :class="{ 'active': activeSection === 'home' }">Domov</a></li>
+        <span><hr class="short"></span>
+        <li><a @click="scrollToSection('about')" :class="{ 'active': activeSection === 'about' }">O nás</a></li>
+        <span><hr class="long"></span>
+      </ul>
+  
+      <!-- logo/middle part -->
+      <img src="../../assets/pictures/logo.svg" alt="logo">
+  
+      <!-- right menu side -->
+      <ul>
+        <span><hr class="long"></span>
+        <li><a @click="scrollToSection('services')" :class="{ 'active': activeSection === 'services' }">Služby</a></li>
+        <span><hr class="short"></span>
+        <li><a @click="scrollToSection('contact')" :class="{ 'active': activeSection === 'contact' }">Kontak</a></li>
+        <span><hr class="long"></span>
+      </ul>
+    </nav>
     </header>
 </template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        activeSection: 'home',
+      };
+    },
+    methods: {
+      scrollToSection(sectionId) {
+        const element = document.getElementById(sectionId);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+          this.activeSection = sectionId;
+        }
+      },
+    },
+  };
+</script>
 
 <style lang="scss">
+    // default styling imports
     @import '../../assets/styles/colors.scss';
     @import '../../assets/styles/navbar.scss';
 </style>
