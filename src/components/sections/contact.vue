ed15d547aad7e46187f0e4df3b6063f914943d15
obs1di4n <80841181+fil1pko@users.noreply.github.com>
2023-08-09 00:12:31 +0200

    contact static done

--- src/components/sections/contact.vue
+++ src/components/sections/contact.vue
@@ -5,11 +5,18 @@
                 <h4>Kontaktujte nás</h4>
                 <div class="longer"></div>
                 <div class="information-wraper">
-                    <div>
-                        <p>ORION-trus s.r.o.</p>
-
-                    </div>
-                    <div></div>
+                    <ul>
+                        <li><p>ORION-trus s.r.o.</p></li>
+                        <li><a href="tel:9876765678">
+                        <img src="../../assets/pictures/phone.svg" alt="phone icon"> 000 000 000</a></li>
+                        <li><a href="tel:9876765678">
+                        <img src="../../assets/pictures/phone.svg" alt="phone icon">000 000 000</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href=""><img src="../../assets/pictures/email.svg" alt=""> WWEEFERLFGEIRGB@GMAIL.COM</a></li>
+                        <li><a href=""><img src="../../assets/pictures/instagram.svg" alt=""> dewwieurfowbf</a></li>
+                        <li><a href=""><img src="../../assets/pictures/facebook.svg" alt=""> dewwieurfowbf</a></li>
+                    </ul>
                 </div>
             </div>
             <div class="right">
@@ -28,6 +35,8 @@
                 </div>
             </div>
         </div>
+
+        <div class="last"><p>© 2023 made by <a href="https://github.com/fil1pko">RU3Y</a></p></div>
     </div>
 </template>
 
